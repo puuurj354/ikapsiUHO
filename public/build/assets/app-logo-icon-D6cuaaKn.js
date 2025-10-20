b3c8b3bae2e53ce37ce1323020fd11f52c7ecbe8
puuurj354 <pooorj14@gmail.com>
2025-10-20 16:47:20 +0800

    ok push

---
+++ public/build/assets/app-logo-icon-D6cuaaKn.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-BuK6L1IX.js";function n(o){return a.jsx("img",{...o,src:"/150px-Logo-baru_UHO.png",alt:"Logo Ikatan Alumni Psikologi Universitas Halu Oleo",className:`${o.className||""} object-contain`})}export{n as A};
---
+++ public/build/assets/input-error-Diwh8xN5.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BuK6L1IX.js";import{a as o}from"./createLucideIcon-C0J_jlnb.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
